a8f8bb1ee0bdc1bd6635e675c6e13dd11390133c
RK6-Dev <105436894+RK6-Dev@users.noreply.github.com>
2022-08-08 10:57:23 +0300

    Add files via upload

---
+++ pro mathfinal.py
@@ -0,0 +1,136 @@
+import math
+def promath():
+    print("welcome to pro math")
+    print("================================")
+    print("press 1 to calculate the area")
+    print("================================")
+    print("press 2 to calculate the perimeter")
+    print("================================")
+    print("press 3 for the calculater")
+    print("================================")
+    print("press 4 to quit")
+    print("================================")
+    userchoice1 = int(input("please choose: "))
+    if userchoice1 == 1:
+        print("perimeter calculater")
+        print("=================================================================")
+        print("press 1 to calculate the area for the square")
+        print("=================================================================")
+        print("press 2 to calculate the area for the rectangle")
+        print("=================================================================")
+        print("press 3 to calculate the area for the triangle")
+        print("=================================================================")
+        print("press 4 to calculate the area for the circle ")
+        print("=================================================================")
+        userareachoice = int(input("please choose: "))
+        if userareachoice == 1:
+            length = float(input("please enter the length"))
+            print("the area of this square is ", length ** 2)
+            again()
+
+        if userareachoice == 3:
+            base = float(input("please enter the base"))
+            length = float(input("please enter the length"))
+            print("the area of this triangle is ", .5 * base * length)
+            again()
+            # rectangle ↓
+        if userareachoice == 2:
+            length = float(input("please enter the length"))
+            base = float(input("please enter the base"))
+            print("the area of this rectangle is ", (base * length))
+            again()
+        if userareachoice == 4:
+            Diameter = int(input("please enter the Diameter for the circle"))
+            print("the area for this circle is ", (math.pi * Diameter))
+            again()
+        else:
+            print("invalid number ")
+            again()
+    if userchoice1 == 2:
+            print("perimeter calculater")
+            print("=================================================================")
+            print("press 1 to calculate the perimeter for the square")
+            print("=================================================================")
+            print("press 2 to calculate the perimeter for the rectangle")
+            print("=================================================================")
+            print("press 3 to calculate the perimeter for the triangle")
+            print("=================================================================")
+            print("press 4 to calculate the area for the circle ")
+            print("=================================================================")
+            userperi = int(input("please choose "))
+            if userperi == 1:
+              length = float(input("please enter the length"))
+              print("the area of this square is ", length * 4)
+              again()
+            elif userperi == 3:
+              side1 = float(input("please enter the length for the first side"))
+              side2 = float(input("please enter the length for the second side"))
+              side3 = float(input("please enter the length for the third side"))
+              print("the perimeter of this triangle is ", side1 + side2 + side3)
+              again()
+            elif userperi == 2:
+              length = float(input("please enter the length"))
+              width = float(input("please enter the width"))
+              print("the perimeter of this rectangle is ", (width + length) * 2)
+              again()
+            elif userperi == 4:
+                 Diameter1: float = float(input("please enter the Diameter"))
+                 print("the perimeter for this circle is ", math.pi * (Diameter1 / 2) * 2)
+                 again()
+            else:
+                print("invalid number")
+                again()
+    if userchoice1 == 3:
+        print("free calculater")
+        print("==========================================")
+        n1 = float(input("please enter you first number "))
+        print("press 1 if you want to +")
+        print("==========================================")
+        print("press 2 if you want to -")
+        print("==========================================")
+        print("press 3 if you want to /")
+        print("==========================================")
+        print("press 4 if you want to *")
+        print("==========================================")
+        print("press 5 if you want to get the square root")
+        print("==========================================")
+        print("press 6 if you want to get the power")
+        callfirt = int(input("please choose the operater "))
+        if callfirt == 1:
+            n2 = float(input("please enter the second number"))
+            print(n1, " + ", n2, '= ', n1 + n2)
+            again()
+        elif callfirt == 2:
+            n2 = float(input("please enter the second number"))
+            print(n1, " - ", n2, '= ', n1 - n2)
+            again()
+
+        elif callfirt == 3:
+            n2 = float(input("please enter the second number"))
+            print(n1, " * ", n2, '= ', n1 / n2)
+            again()
+        elif callfirt == 4:
+            n2 = float(input("please enter the second number"))
+            print(n1, " * ", n2, '= ', n1 * n2)
+            again()
+        elif callfirt == 6:
+            n2 = float(input("please enter the second number"))
+            print(n1, '**', n2, '+', n1 ** n2)
+            again()
+        elif callfirt == 5:
+            print("the square root for ", n1, math.sqrt(n1))
+            again()
+        else:
+            print("invalid number")
+            again()
+        # *---------------------------------------
+def again():
+    againi = input("would you lik to try again? y/n")
+    if againi == 'y':
+        promath()
+    else:
+        print("thank you for using my program")
+
+
+
+promath()
